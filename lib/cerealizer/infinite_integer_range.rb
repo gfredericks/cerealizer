e9fe7ef124632b00b78b71c14858a07f7e1b6312
gary <gary@crunchbang.(none)>
2010-06-03 17:19:43 -0400

    Added some things I guess

--- lib/cerealizer/infinite_integer_range.rb
+++ lib/cerealizer/infinite_integer_range.rb
@@ -1,5 +1,12 @@
 module Cerealizer
   class InfiniteIntegerRange < Base
-    def initialize()
+    # I can't do this in any nice way :(
+    def initialize(opts)
+      min = opts[:min] || 0
+      max = opts[:max] || :infinity
+      unless([min,max].include?(:infinity))
+        
+      end
+    end
   end
 end
