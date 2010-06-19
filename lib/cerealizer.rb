fc77e71664d7304b734f3155b4007c5e1d6bc49b
Gary Fredericks <fredericksgary@gmail.com>
2010-06-19 15:21:32 -0500

    Tests passing for two base domains

--- lib/cerealizer.rb
+++ lib/cerealizer.rb
@@ -1,11 +1,14 @@
 module Cerealizer
   VERSION = "0.0.1"
 end
+=begin
 require 'cerealizer/exceptions.rb'
 require 'cerealizer/base'
 require 'cerealizer/wrapper'
 require 'cerealizer/finite_integer_range'
 require 'cerealizer/infinite_integer_range'
 require 'cerealizer/binary'
+=end
+require 'cerealizer/domain'
 
 include Cerealizer
--- lib/cerealizer/domain.rb
+++ lib/cerealizer/domain.rb
@@ -2,13 +2,71 @@ require 'set'
 
 module Cerealizer
   class Domain
-    NATURALS = Domain.new(
-      lambda{|ob|self.is_n?(ob)},
-      lambda{|x|x}, 
-      lambda{|x|x},
-      :aleph_null)
+    attr_accessor :cardinality
+
+    private 
+    def initialize(domain, to_n, from_n, cardinality)
+      @domain=domain
+      @to_n=to_n
+      @from_n=from_n
+      @cardinality=cardinality
+    end
+
+    public
+
+    def self.string(alphabet)
+      alphabet = alphabet.split("") if(alphabet.class == String)
+      check_that("Alphabet must consist of single-character strings") do
+        alphabet.all? do |char|
+          char.class == String and char.length == 1
+        end
+      end
+      alphabet = alphabet.uniq
+      alphabet_set = Set.new(alphabet)
+      Domain.new(
+        lambda{|ob|ob.class == String and ob.split("").all?{|c|alphabet_set.include?(c)}},
+        lambda{|s|
+          possible = 1
+          digits = 0
+          n = 1
+          until(digits == s.length)
+            n += possible
+            digits += 1
+            possible *= alphabet.length
+          end
+          mult = 1
+          i = s.length-1
+          while(i >= 0)
+            n += mult*alphabet.index(s[i..i])
+            mult *= alphabet.length
+            i-=1
+          end
+          n
+        },
+        lambda{|n|
+          return "" if n==1
+          n-=1
+          digits = 1
+          possible = alphabet.length
+          while(n > possible)
+            n -= possible
+            possible *=alphabet.length
+            digits += 1
+          end
+          n-=1
+          s = ""
+          while(n > 0)
+            s = alphabet[n % alphabet.length] + s
+            n /= alphabet.length
+          end
+          alphabet[0]*(digits - s.length) + s
+        },
+        :aleph_null)
+          #n + s.to_i(3)
+    end
 
     def self.finite_disjunction(set)
+      check_that("set must be enumerable"){set.class < Enumerable}
       set = set.to_set
       a = set.to_a
       Domain.new(
@@ -23,27 +81,36 @@ module Cerealizer
     end
 
     def to_n(el)
-      raise "Argument not in domain" unless @domain.include?(el)
+      Domain.check_that("Argument must be in domain"){@domain.call(el)}
       @to_n.call(el)
     end
 
     def from_n(n)
-      raise "That's not a natural number" unless is_n?(n)
-      raise "#{n} is larger than the cardinality of #{@cardinality}" unless
+      Domain.check_that("Argument must be a natural number"){Domain.is_n?(n)}
+      Domain.check_that("Argument must be within cardinality of domain") do
         @cardinality == :aleph_null or n <= @cardinality
+      end
       @from_n.call(n)
     end
     
     private
-    def initialize(domain, to_n, from_n, cardinality)
-      @domain=domain
-      @to_n=to_n
-      @from_n=from_n
-      @cardinality=cardinality
-    end
-
     def self.is_n?(ob)
       [Fixnum, Bignum].include?(ob.class) and ob > 0
     end
+
+    def self.check_that(message)
+      raise message unless yield
+    end
+
+    public 
+
+    NATURALS = Domain.new(
+      lambda{|ob|self.is_n?(ob)},
+      lambda{|x|x}, 
+      lambda{|x|x},
+      :aleph_null)
+
+    ASCII = self.string(" !\"\#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")
+
   end
 end
